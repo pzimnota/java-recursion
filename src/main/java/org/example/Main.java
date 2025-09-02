3adec4c1aa1f989301c6e89de5a7e401461f2b70
Paweł Zimnota <pawel.zimnota@protonmail.com>
2025-09-02 21:09:03 +0200

    Added test for Multi-MultiFolder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,26 +7,17 @@ import java.util.Optional;
 public class Main {
     public static void main(String[] args) {
 
-        SimpleFolder simpleFolder1 = new SimpleFolder("first", "MEDIUM");
-        SimpleFolder simpleFolder2 = new SimpleFolder("second", "LARGE");
-        SimpleFolder simpleFolder3 = new SimpleFolder("third", "SMALL");
+        FileCabinet fileCabinet = getFileCabinet();
 
-        List<Folder> folders = new ArrayList<>();
-        folders.add(simpleFolder1);
-        folders.add(simpleFolder2);
-
-        CompositeFolder compositeFolder = new CompositeFolder("firstComposite", "SMALL", folders);
-        List<Folder> topLevelFolders = new ArrayList<>();
-        topLevelFolders.add(compositeFolder);
-
-        topLevelFolders.add(simpleFolder3);
-        FileCabinet fileCabinet = new FileCabinet(topLevelFolders);
-
-        fileCabinet.traverseActionFolders(topLevelFolders, folder -> System.out.println(folder.getName()));
         Optional<Folder> foundFolder = fileCabinet.findFolderByName("third");
-
         foundFolder.ifPresent(folder -> System.out.println("Found folder: " + folder.getName()));
-        System.out.println(fileCabinet.findFoldersBySize("SMALL"));
+        System.out.println("Present? " + fileCabinet.findFolderByName("missing").isPresent());
+
+        List<Folder> smallFolders = fileCabinet.findFoldersBySize("SMALL       ");
+        System.out.println("Small folders: " + smallFolders);
+        System.out.println(fileCabinet.findFoldersBySize("tiny"));
+
+
         System.out.println("Number of all folders: " + fileCabinet.count());
 
 
@@ -37,5 +28,35 @@ public class Main {
         System.out.println(empty.findFoldersBySize("LARGE"));
 
 
+    }
+
+
+
+    private static FileCabinet getFileCabinet() {
+        SimpleFolder simpleFolder1 = new SimpleFolder("first", "MEDIUM");
+        SimpleFolder simpleFolder2 = new SimpleFolder("second", "LARGE");
+        SimpleFolder simpleFolder3 = new SimpleFolder("third", "SMALL");
+        SimpleFolder simpleFolder4 = new SimpleFolder("forth", "MEDIUM");
+
+        List<Folder> folders = new ArrayList<>();
+        folders.add(simpleFolder1);
+        folders.add(simpleFolder2);
+        CompositeFolder compositeFolder = new CompositeFolder("firstComposite", "SMALL", folders);
+
+        List<Folder> folders2 = new ArrayList<>();
+        folders2.add(simpleFolder4);
+        CompositeFolder compositeFolderInside = new CompositeFolder("compositeInside", "LARGE", folders2);
+
+        List<Folder> folders3 = new ArrayList<>();
+        folders3.add(compositeFolderInside);
+
+        CompositeFolder compositeFolderTop = new CompositeFolder("compositeTop", "LARGE", folders3);
+
+        List<Folder> topLevelFolders = new ArrayList<>();
+        topLevelFolders.add(compositeFolder);
+        topLevelFolders.add(simpleFolder3);
+        topLevelFolders.add(compositeFolderTop);
+
+        return new FileCabinet(topLevelFolders);
     }
 }
